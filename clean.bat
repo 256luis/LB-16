0e78b0419dc0fc0004c459d4ff3026aab4b1c914
luisbanez <luismbanez@gmail.com>
2022-09-03 12:45:19 +0800

    added clean.bat

---
+++ clean.bat
@@ -0,0 +1,4 @@
+@echo off
+
+if exist *.exe del /q *.exe
+if exist build\*.o* del /q build\*.o*
--- src/main.c
+++ src/main.c
@@ -8,8 +8,6 @@ int main(int argc, char** argv)
 {
     INSIST(argc > 1, "no file specified\n");
 
-    // sdl stuff
-    
     CPU* cpu = cpu_init();
     Instruction* program = load_program(argv[1]);
 
